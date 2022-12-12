9de69ee2ce3f9feb42869d5190b8f65eacf2e2c5
v-kdemir <kdemir@valven.com>
2022-12-12 15:30:48 +0300

    python

---
+++ commit.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import git
+import os
+
+# To initiate new Git repo in the mentioned directory
+os.chdir('/home/kdemir/Documents/py')
+print("Current working directory: {0}".format(os.getcwd()))
+
+#import pdb; pdb.set_trace();
+# Define the name of the file to read from
+filename = "ldap_payload"
+pyfile="test.py"
+
+# Open the file for reading
+filehandle = open(filename, 'r+')
+while True:
+    # read a single line
+    line = filehandle.readline()
+    if not line:
+        break
+    print( line)
+    with open(pyfile, "a+") as f:
+        f.write("a")
+    os.system('git add test.py')
+    os.system('git commit -m' + '"' + line +'/'+ '"')
+    os.system('git push')
+    f.close()
+
+# Close the pointer to that file
+filehandle.close()
---
+++ pr.py
@@ -0,0 +1,30 @@
+import git
+import os
+
+os.chdir('/home/kdemir/Documents/py')
+print("Current working directory: {0}".format(os.getcwd()))
+
+filename = "command_injection_payload"
+pyfile = "test.py"
+
+filehandle = open(filename, 'r+')
+while True:
+    lines = filehandle.readlines()
+    count=900
+    if not lines:
+        break
+#    import pdb; pdb.set_trace();
+    for line in lines:
+        count+=1
+        os.system('git fetch --all')
+        os.system('git checkout ' + str(count))
+        os.system('git pull')
+        with open(pyfile, "a+") as f:
+            f.write("a")
+        os.system('git add test.py')
+        os.system('git commit -m' +"'" +line +"/" +"'")
+        os.system('git push --set-upstream origin ' + str(count))
+        print('gh pr create -B main -H  ' + str(count) + ' --title ' + '\"'+line + '\"' +" " +' --body '+ '\"'+  line + '\"')
+        os.system('gh pr create -B main -H  ' + str(count) + ' --title ' + '\"'+line +'\"' +" "+' --body '+ '\"'+  line + '\"' )
+f.close()
+filehandle.close()
